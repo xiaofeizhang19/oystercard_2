ac611b58b9147cb8569cf43188f4056a314a578c
karlo <deguzmankarlo@gmail.com>
2019-08-27 15:10:04 +0100

    Add test for top up method
    Oyster card class responds to top up method by increasing balance amount.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,8 +2,14 @@ require 'oystercard'
 
 describe Oystercard do
 
-  it 'will start with a balance of 0' do
-    expect(subject.balance).to eq 0
-  end
+  describe 'card balance' do
 
+    it 'will start with a balance of 0' do
+      expect(subject.balance).to eq 0
+    end
+
+    it 'will increase balance with top up' do
+      expect(subject.top_up(5)).to eq 5
+    end
+  end
 end
