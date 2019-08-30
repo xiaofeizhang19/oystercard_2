1ab214d19c1bb6bf1c59bbafb0803d3b137edef1
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-08-30 16:20:04 +0100

    Test adding journey to journeylog

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -1,20 +1,33 @@
+
+
 class JourneyLog
 
-  attr_reader :journey
-  attr_reader :entry_station
+  attr_reader :entry_station, :exit_station, :journey_class
 
-  def initialize(journey_class: )
+  def initialize(journey_class = Journey)
     @journey_class = journey_class
+    @journeys = []
   end
 
   def start(entry_station)
-    @entry_station = entry_station
+    current_journey.enter(entry_station)
+    @journeys << current_journey
+  end
+
+  def finish(exit_station)
+    current_journey.exit(exit_station)
+    @journeys << current_journey
+    @current_journey = nil
+  end
+
+  def journeys
+    @journeys.dup
   end
 
   private
 
   def current_journey
-    @current_journey.initialize(entry_station)
+    !!@current_journey ? @current_journey : @journey_class.new  
   end
 end
 
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -2,13 +2,23 @@ require 'journeylog'
 
 describe JourneyLog do
 
+  let (:journey) { double :journey }
+  let (:journey_class) { double :journey_class, new: journey}
   let (:entry_station) { double :entry_station }
+  let (:exit_station) { double :exit_station }
 
-  subject { JourneyLog.new(journey_class: Journey.new) }
+  subject { described_class.new(journey_class) }
 
   describe '#start' do
-    it 'should start a new journey with an entry station' do
-      expect(subject.start(entry_station)).to eq(entry_station)
+
+  end
+
+  describe '#finish' do
+    it 'should finish a journey with an exit station' do
+      allow(journey_class).to receive(:new).and_return journey
+      subject.start(entry_station)
+      # subject.finish(exit_station)
+      expect(subject.journeys).to include(journey)
     end
   end
 end
